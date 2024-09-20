bbf977678c11133b2d9e8ad928a62cd14ce0e84d
njnguyen <njnguyen.001@gmail.com>
2024-09-19 21:25:45 -0600

    Added code to empty jsx files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="foot-container">
+            <p className="foot-title">Let's chat!</p>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/pages/blog/index.js
@@ -0,0 +1,4 @@
+export default function Blog() {
+    return <h1>Blog Page Coming Soon!</h1>;
+}
+  
