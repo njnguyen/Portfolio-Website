67f4fa25cf5dff3c40e6e1123235565d6689aba4
njnguyen <njnguyen.001@gmail.com>
2024-09-23 23:01:32 -0600

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+import { FiMail } from 'react-icons/fi'
+
 const Footer = () => {
     return (
         <div className="foot-container">
             <p className="foot-title">Let&#39;s chat!</p>
+            <div className="foot-icons">
+                <a href="mailto:nj.nguyen0205@gmail.com" target="_blank"><FiMail size={20} /></a>
+                <a href="https://www.linkedin.com/in/njnguyen/" target="_blank"><FaLinkedinIn size={20} /></a>
+                <a href="https://github.com/njnguyen" target="_blank"><FaGithub size={20} /></a>
+            </div>
+            <p className="copyright">Copyright &copy; 2024 Jackie Nguyen</p>
         </div>
     )
 }
